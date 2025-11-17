6778e49212d9d307430ec423fd4e64745fecd0e0
Meiez <meiezbenromdhane@gmail.com>
2025-11-17 17:45:51 +0100

    Updated terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,27 @@ resource "aws_security_group" "devops_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = -1
+    to_port          = -1
+    protocol         = "icmp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
